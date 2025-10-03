ca4de7ae05d6c8c48b8722524612740d64ec2a11
enesugurlu <enesugurlu1684@gmail.com>
2025-10-03 10:32:21 +0300

    variables and first c code

---
+++ lesson1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    ///printf("Hello, World!");
+    ///printf("This is my first C code ");
+    
+    int num1,num2;
+
+    num1 = 4;
+    num2 = 5;
+
+    int sum = num1 + num2;
+    int multiply = num1 * num2;
+
+    printf("This result is: %d ",sum);
+
+    printf("Operation of multiply result is %d",multiply);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
